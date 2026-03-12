8a500198e964fa1ab15e8c1589475c95dbd8e6f5
Carlos Cesar <carloscesarct03@gmail.com>
2026-03-12 16:50:02 -0300

    Update HeroSection.tsx

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -58,7 +58,7 @@ export default function HeroSection() {
                 Seu filho clica no link… e começa o desafio.
               </p>
               <a 
-                href="https://payfast.greenn.com.br/u8v74a5" 
+                href="https://payfast.greenn.com.br/redirect/270668" 
                 className="block w-full text-center bg-emerald-500 hover:bg-emerald-400 text-white font-bold text-xl py-5 px-8 rounded-full shadow-[0_0_20px_rgba(16,185,129,0.4)] transition-all transform hover:scale-105"
               >
                 Quero Acesso Imediato
